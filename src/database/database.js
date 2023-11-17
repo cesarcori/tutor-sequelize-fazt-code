d0fdc7d384a942f00578ace79e4912f7d84ad905
jcesar <jcesarcori@gmail.com>
2023-11-16 23:56:54 -0400

    feat: test de la conextion con la base de datos

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize'
 export const sequelize = new Sequelize(
   'projectsdb',
   'postgres',
-  'mysecretpassword', 
+  'postgres', 
   {
   host: 'localhost',
   dialect: 'postgres'
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import app from "./app.js";
+import {sequelize} from './database/database.js'
 
-const puerto = 4000
+async function main() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    const puerto = 5001
+    app.listen(puerto)
+    console.log("Server esta escuchando on", puerto)    
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
-app.listen(puerto)
-
-console.log("Server esta escuchando on", puerto)
+main()
