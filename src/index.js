dcf7bb7015c9fc2f2210420c5af1e62564971a30
jcesar <jcesarcori@gmail.com>
2023-11-18 20:02:25 -0400

    feat: tabla project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import app from "./app.js";
 import {sequelize} from './database/database.js'
 
+import './models/Project.js'
+
 async function main() {
   try {
-    await sequelize.authenticate();
+    await sequelize.sync({force: true});
+    // await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-    const puerto = 5001
+    const puerto = 4000
     app.listen(puerto)
     console.log("Server esta escuchando on", puerto)    
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
-}
+} 
 
 main()
