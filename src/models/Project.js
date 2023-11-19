03dd56c9e92a5ce62eb1f1cdd15193be3a8d1123
jcesar <jcesarcori@gmail.com>
2023-11-18 20:24:15 -0400

    feat: realcion uno a muchos

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,5 +1,6 @@
 import {DataTypes} from 'sequelize';
 import {sequelize} from '../database/database.js';
+import {Task} from './Task.js'
 
 export const Project = sequelize.define('projects', {
   id: {
@@ -18,4 +19,14 @@ export const Project = sequelize.define('projects', {
   },
 }, {
   timestamps: true
+});
+
+Project.hasMany(Task, {
+  foreignKey: 'projectId',
+  sourceKey: 'id'
+})
+
+Task.belongsTo(Project, {
+  foreignKey: 'projectId',
+  targetId: 'id'
 })
